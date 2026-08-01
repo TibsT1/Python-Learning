05ea1bed1aaf30b46c180d439c611fe03102a9e8
TibsT1 <tiberiumtoma@gmail.com>
2026-08-01 23:29:23 +0100

    Completed lesson 3
    Completed lesson 3 which teaches me about strings and putting them together in sentences using sentence concatenation, and also converting integers into strings using str().

---
+++ 01 – Python Fundamentals/lesson-03-strings.py
@@ -0,0 +1,26 @@
+# 1. Store your first name.
+first_name = "Tibi"
+
+# 2. Store your target role.
+target_role = "Cybersecurity Analyst"
+
+# 3. Store the number of weekly study hours.
+weekly_study_hours = 14
+
+# 4. Create a heading by repeating = twenty times.
+heading = "=" * 20
+
+# 5. Create one sentence using string concatenation that includes your name and target role.
+full_sentence = "My name is " + first_name + " and I want to work as a " + target_role
+
+# 6. Print the heading.
+print(heading)
+
+# 7. Print the sentence.
+print(full_sentence)
+
+# 8. Print the weekly study hours with a clear label.
+print("I will be studying for " + str(weekly_study_hours) + " hours per week to achieve this target role!")
+
+# 9. Print the heading again.
+print(heading)
