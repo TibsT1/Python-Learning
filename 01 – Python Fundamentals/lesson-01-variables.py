59db8a2ac125746a87fa3f3330f6d36d774201df
TibsT1 <tiberiumtoma@gmail.com>
2026-08-01 01:57:48 +0100

    Completed first lesson
    I have completed my first lesson on python which was about variables and data types.

---
+++ 01 – Python Fundamentals/lesson-01-variables.py
@@ -0,0 +1,16 @@
+my_name = "Tibi"
+my_age = 22
+completed_degree = True
+target_job = "cybersecurity analyst"
+weekly_study_hours = 14
+
+print(my_name, my_age, completed_degree, target_job, weekly_study_hours)
+print(type(my_name))
+print(type(my_age))
+print(type(completed_degree))
+print(type(target_job))
+print(type(weekly_study_hours))
+
+weekly_study_hours = weekly_study_hours + 2
+
+print(weekly_study_hours)
